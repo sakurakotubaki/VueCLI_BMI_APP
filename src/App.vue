90a1073961911cf318be5a01249289107d321ab8
hashi <bluepatissere0310@iclud.com>
2021-02-05 00:32:04 +0900

    シングルページアプリケーションのBMIアプリを作成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>{{ BMI }}</h1><hr>
+   <p>BMI（ボディマス指数 Body Mass Index：肥満指数）、標準体重を計算してみよう！<br>
+       計算式はBMI＝<span id="s1">体重(kg) ÷ {身長(m) Ｘ 身長(m)}</span><br>
+       BMI=<span id="s2">mass(lb) ÷ {heigiht(in) x height(in)} x 703 </span>(ヤード・ポンド法)</p>
+   <input v-model.number="weight" type="number">kg<br>
+   <input v-model.number="height" type="number">cm<br>
+   <p class="standard">あなたの標準体重{{ calc }}kg</p><br>
+   <p class="sm">小数点を切り捨てた数値{{ mf }}kg</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      BMI: 'BMI値計算アプリ',
+      weight: 0,
+      height: 0
+    }
+  },
+  computed: {
+    calc: function() {
+      let w = this.weight
+      let h = this.height * 2
+      return  h / w * 703
+    },
+    mf: function() {
+      return Math.floor(this.calc)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1 {
+  color: gray;
+}
+
+#s1 {
+  color: rgb(83, 41, 237);
+}
+
+#s2 {
+  color: rgba(242, 44, 44, 0.824);
+}
+
+.standard {
+  color: rgba(242, 44, 44, 0.824);
+  border-bottom: solid 2px gray;
+}
+
+.sm {
+  color: blue;
 }
 </style>
